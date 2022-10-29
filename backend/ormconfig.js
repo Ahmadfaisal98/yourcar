876ff391246eb3f8bb14ede0eaa124876515db85
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-10-29 07:18:30 +0700

    update orm config for dev

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,13 +1,14 @@
 module.exports = [
   {
     name: 'dev',
-    type: 'mysql',
-    host: 'mysqldb',
-    port: 3306,
-    username: 'root',
-    password: 'pass123',
-    database: 'yourcar',
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     entities: ['dist/**/entities/*{.ts,.js}'],
+    ssl: { rejectUnauthorized: false },
     synchronize: true,
     migrations: ['dist/migrations/*{.ts,.js}'],
     cli: {
